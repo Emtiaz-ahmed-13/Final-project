c2bbc5a7684d8072020159f44867b541d4eee8ca
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-08-19 04:05:14 +0600

    finish with order page error
    the problem is i can't make the order page default as a "salad"

--- frontend/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ frontend/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,27 +1,16 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import UseMenu from "../../../Hooks/UseMenu";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      })
-      .catch((error) => {
-        console.error("Error fetching menu data:", error);
-      });
-  }, []);
+  const [menu] = UseMenu();
+  const popular = menu.filter((item) => item.category === "popular");
 
   return (
     <section>
       <SectionTitle subheading="From Our Menu" heading="Popular Items" />
       <div className="grid md:grid-cols-2 gap-10">
-        {menu.map((item) => (
+        {popular.map((item) => (
           <MenuItem key={item._id} item={item} />
         ))}
       </div>
--- frontend/src/Pages/Menu/Menu.jsx
+++ frontend/src/Pages/Menu/Menu.jsx
@@ -1,17 +1,45 @@
 import { Helmet } from 'react-helmet-async';
 
+
+import menuImg from '../../assets/menu/dessert-bg.jpeg';
+import dessertImg from '../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../assets/menu/pizza-bg.jpg';
+import saladImg from '../../assets/menu/salad-bg.jpg';
+import soupImg from '../../assets/menu/soup-bg.jpg';
+
+
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import MenuCategory from './MenuCategory/MenuCategory';
+import UseMenu from '../../Hooks/UseMenu';
+import Cover from '../Shared/Cover/Cover';
+
+
 const Menu = () => {
+    const [menu] = UseMenu();
+
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
+            {/* main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
 
-          <Helmet>
-            <title>Bistro | Menu</title>
-          </Helmet>
-          <h2>
-            Menu..
-          </h2>
+            {/* offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items  */}
+            <MenuCategory items={desserts} title="dessert" img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
         </div>
     );
 };
 
-export default Menu;  
+export default Menu;
