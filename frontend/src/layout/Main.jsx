b0211c71aad53d846b83ba23a3815e22325b7488
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-08-21 23:12:21 +0600

    doing login page

--- frontend/src/layout/Main.jsx
+++ frontend/src/layout/Main.jsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Footer from "../Pages/Shared/Footer/Footer";
 import Header from "../Pages/Shared/Header/Header";
 
 const Main = () => {
+    const location =useLocation();
+    console.log(location);
+    const noHeaderFooter=location.pathname.includes('login')
     return (
         <div>
-            <Header />
+            {noHeaderFooter || <Header />}
             <Outlet />
-            <Footer />
+            {noHeaderFooter || <Footer />}
         </div>
     );
 };
