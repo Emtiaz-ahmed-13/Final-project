96a4f10bb690794362d6a921830a4a65a83f4293
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-08-23 16:19:43 +0600

    doing auth with a lot off error

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,66 +1,128 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Helmet } from "react-helmet-async";
-import { LoadCanvasTemplate } from 'react-simple-captcha';
+import { Link } from "react-router-dom";
+import {
+  LoadCanvasTemplate,
+  loadCaptchaEnginge,
+  validateCaptcha,
+} from "react-simple-captcha";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
-    const handleLogin = event=>{
-        event.preventDefault();
-        const form = event.target;
-        const email =form.email.value;
-        const password = form.password.value;
-        console.log(email,password);
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
 
-    useEffect(()=>{
-        loadCaptachEngine(6);
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signIn(email, password).then((result) => {
+      const user = result.user;
+      console.log(user);
+    });
+  };
 
-    },[])
-    return (
-        <>
-            <Helmet>
-                <title>Bistro Boss | Login</title>
-            </Helmet>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col md:flex-row-reverse">
-                    <div className="text-center md:w-1/2 lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleLogin} className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <LoadCanvasTemplate />
-                                </label>
-                                <input  type="text" name="captcha" placeholder="type the captcha above" className="input input-bordered" />
+  const handleValidateCaptcha = (event) => {
+    event.preventDefault();
+    const user_captcha_value = captchaRef.current.value;
 
-                            </div>
-                            {/* TODO: make button disabled for captcha */}
-                            <div className="form-control mt-6">
-                                <input disabled={false} className="btn btn-primary" type="submit" value="Login" />
-                            </div>
-                        </form>
-                        <p><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
-                        <SocialLogin></SocialLogin>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+      alert("Invalid Captcha! Please try again.");
+    }
+  };
+
+  const {} = useContext(AuthContext);
+
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  return (
+    <>
+      <Helmet>
+        <title>Bistro Boss | Login</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col md:flex-row-reverse">
+          <div className="text-center md:w-1/2 lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Captcha</span>
+                </label>
+                <LoadCanvasTemplate />
+                <input
+                  ref={captchaRef}
+                  type="text"
+                  name="captcha"
+                  placeholder="type the captcha above"
+                  className="input input-bordered mt-2"
+                />
+                <button
+                  onClick={handleValidateCaptcha}
+                  className="btn btn-outline btn-xs mt-2"
+                >
+                  Validate
+                </button>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  disabled={disabled}
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+            <p>
+              <small>
+                New Here? <Link to="/signup">Create an account</Link>
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
-}
-export default Login ;
+
+export default Login;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
-import '../src/index.css';
-import router from './Routes/Routes';
+import "../src/index.css";
+import router from "./Routes/Routes";
+import AuthProvider from "./providers/AuthProvider";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <HelmetProvider >
-   <div className="max-w-screen-xl mx-auto">
-   <RouterProvider router={router} />
-   </div>
-   </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>
-)
+);
